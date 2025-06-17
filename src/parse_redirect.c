9803420ec1f58b3528cf5211ad7288f15f196388
makpolatt <emreakpolat3@gmail.com>
2025-06-17 15:26:37 +0300

    başta ve sonda hatalı redirect kullanımında hata döndürecek şekilde kod yenilendi / ayrıca fazla redirect kullanımında da hata döndürecek şekilde işlem yapıldı

--- src/parse_redirect.c
+++ src/parse_redirect.c
@@ -6,14 +6,12 @@
 /*   By: makpolat <makpolat@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 14:58:12 by makpolat          #+#    #+#             */
-/*   Updated: 2025/06/16 17:20:50 by makpolat         ###   ########.fr       */
+/*   Updated: 2025/06/17 15:24:13 by makpolat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-
-
-int ft_strcmp(const char *s1, const char *s2) // dünyanın en basit fonksiyonunun amcası falan
+int ft_strcmp(const char *s1, const char *s2)
 {
     while (*s1 && *s2 && *s1 == *s2)
     {
@@ -23,7 +21,7 @@ int ft_strcmp(const char *s1, const char *s2) // dünyanın en basit fonksiyonun
     return ((unsigned char)*s1 - (unsigned char)*s2);
 }
 
-static t_command *create_node(void) // yeni node oluşyurup sonra ilerde başımız ağrımasın diye NULL ile başlatıyoruz parametreleri
+static t_command *create_node(void)
 {   
     t_command *node = (t_command *)malloc(sizeof(t_command));
     if (!node) return NULL;
@@ -38,21 +36,24 @@ static t_command *create_node(void) // yeni node oluşyurup sonra ilerde başım
     return node;
 }
 
-void print_command_list(t_command *cmd) // Bu Fonksiyon gelen nodeları DOğru bir şekilde ekliyo mu test etmek için silmeyi unutma !!!!
+void print_command_list(t_command *cmd)
 {
     int i = 0;
-
+    int j = 0;
     while (cmd)
     {
         printf("Komut %d:\n", i);
         
         if (cmd->args)
         {
+            j = 0;
             printf("  Argümanlar:\n");
-            for (int j = 0; cmd->args[j]; j++)
+            while (cmd->args[j])
+            {
                 printf("    - %s\n", cmd->args[j]);
+                j++;
+            }
         }
-
         if (cmd->input_fd)
             printf("  Girdi yönlendirme (<): %s\n", cmd->input_fd);
         if (cmd->output_fd)
@@ -68,14 +69,9 @@ void print_command_list(t_command *cmd) // Bu Fonksiyon gelen nodeları DOğru b
 }
 
 
-static void handle_redirection(t_command *node, char **tokens, int *j) // bu fonksiyonda gelen redirectlerden sonraki token yani hedef dosyayı buluyoruz 'ls > out.txt'
-{                                                                       //burada bulacağı yer "out.txt" //mustafaya binlerce teşekkür burdan çünkü kafamda böyle bir fonksiyonun canlanmasını sağladı
-    if (!tokens[*j + 1])
-    {
-        printf("minishell: syntax error near unexpected token `newline'\n");
-        return;
-    }
 
+static void handle_redirection(t_command *node, char **tokens, int *j)
+{
     if (ft_strcmp(tokens[*j], "<<") == 0)
         node->heredoc_fd = ft_strdup(tokens[++(*j)]);
     else if (ft_strcmp(tokens[*j], ">>") == 0)
@@ -86,13 +82,29 @@ static void handle_redirection(t_command *node, char **tokens, int *j) // bu fon
         node->output_fd = ft_strdup(tokens[++(*j)]);
 }
 
-static int is_redirection(char *token) // bu zaten dünyanın en basit fonksiyonu bakınca anlamıyosan köyüne dön
+static int is_redirection(char *token)
 {
     return (ft_strcmp(token, "<<") == 0 || ft_strcmp(token, ">>") == 0 ||
             ft_strcmp(token, "<") == 0 || ft_strcmp(token, ">") == 0);
 }
 
-static int ft_arg_count(char **tokens) // adından da anlaşılacağı üzere
+static int validate_redirection(char **tokens, int index)
+{
+    if (!tokens[index + 1])
+    {
+        printf("minishell: syntax error near unexpected token `newline'\n");
+        return (0);
+    }
+    if (is_redirection(tokens[index + 1]))
+    {
+        printf("minishell: syntax error near unexpected token `%s'\n", tokens[index + 1]);
+        return (0);
+    }
+    
+    return (1);
+}
+
+static int ft_arg_count(char **tokens)
 {
     int i;
     int arg_count;
@@ -100,9 +112,13 @@ static int ft_arg_count(char **tokens) // adından da anlaşılacağı üzere
     i = 0;
     arg_count = 0;
     while (tokens[i])
-    {
+    {        
         if (is_redirection(tokens[i]))
+        {
+            if (!validate_redirection(tokens, i))
+                return (-1); // Hata durumu
             i += 2;
+        }
         else
         {
             arg_count++;
@@ -112,49 +128,73 @@ static int ft_arg_count(char **tokens) // adından da anlaşılacağı üzere
     return (arg_count);
 }
 
-static void parse_argv(t_command *node, char **tokens) // dostum şimdi bu fonksiyon gelen argüman sayısına göre yer ayırıyo ve eğer o argümanların içinde redirect varsa
-{                                                      // redirect varsa handle_redirect fonksiyonunda onların adını saklıyor dehşet bişey yani eğer yoksa da argümanlara strdup ile yer ayırıp döndürüyor
+static int parse_argv(t_command *node, char **tokens)
+{
     int i = 0;
     int arg_count;
     
-    arg_count = ft_arg_count(tokens);
-    if (arg_count > 0)
+    if (tokens[0] && is_redirection(tokens[0]))
     {
-        node->args = (char **)ft_calloc(arg_count + 1, sizeof(char *));
-        i = 0;
-        arg_count = 0;
-        while (tokens[i])
-        {
-            if (is_redirection(tokens[i]))
-                handle_redirection(node, tokens, &i);
-            else
-                node->args[arg_count++] = ft_strdup(tokens[i]);
-            i++;
-        }
+        printf("minishell: syntax error near unexpected token `%s'\n", tokens[0]);
+        return (0);
     }
+    arg_count = ft_arg_count(tokens);
+    if (arg_count == -1) // Hata durumu
+        return (0);
+    if (arg_count == 0)
+        return (1);
+    
+    node->args = (char **)ft_calloc(arg_count + 1, sizeof(char *));
+    if (!node->args)
+        return (0);
+    i = 0;
+    arg_count = 0;
+    while (tokens[i])
+    {
+        if (is_redirection(tokens[i]))
+        {
+            handle_redirection(node, tokens, &i);
+        }
+        else
+        {
+            node->args[arg_count++] = ft_strdup(tokens[i]);
+        }
+        i++;
+    }
+    return (1);
 }
 
-void add_node(char **shell) // bu fonksiyon gelen pipe a göre ayrılan blokları nodelara eklemek için
-{                               //TODO shell ve token değişkenlerini hala daha free etmedik unutma
+void add_node(char **shell)
+{
     t_command *head = NULL;
     t_command *curr = NULL;
     t_command *node = NULL;
     char **tokens;
     int i;
-
+    
     i = 0;
     while (shell[i])
     {
         node = create_node();
         tokens = ft_split(shell[i], ' ', 0, 0);
+
+        // parse_argv artık hata durumu döndürüyor
+        if (!parse_argv(node, tokens))
+        {
+            // Hata durumunda temizlik yap ve çık
+            free(node);
+            return;
+        }
         
-        parse_argv(node, tokens);
         if (!head)
             head = node;
         else
             curr->next = node;
         curr = node;
+        
+        // tokens'ı serbest bırakma fonksiyonu eklenecek
         i++;
     }
+    
     print_command_list(head);
 }
