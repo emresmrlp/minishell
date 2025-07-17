6c8bfbcc8576a4092037168e7784be4b0717f85a
makpolatt <emreakpolat3@gmail.com>
2025-07-17 16:19:07 +0300

    include backup

---
+++ include/minishell.h
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makpolat <makpolat@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 16:51:50 by ysumeral          #+#    #+#             */
+/*   Updated: 2025/07/17 16:00:38 by makpolat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../external/libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <unistd.h>
+# include <stdio.h>
+
+typedef struct s_envp {
+	char            *key;
+	char            *value;
+	struct s_envp  *next;
+} t_envp;
+
+typedef struct s_command {
+	char                **args;     
+	char                *input_fd;   // >
+	char                *output_fd; // <
+	char                *append_output_fd; // >>
+	char                *heredoc_fd; // <<
+	t_envp              *env_list;    // Environment linked list
+	int                 dollar;
+	struct s_command    *next;
+} t_command;
+
+t_envp	*env_head(char **envp);
+char	*find_env(t_envp *head, char *search);
+void	export_add(t_command *iter);
+t_envp	*env_head(char **envp);
+void	read_input(t_envp *env_list);
+void	split_built_in(t_command *head);
+void	parse_command(char *command_line, t_envp *env_list);
+void	add_node(char **shell, t_envp *env_list);
+void	error(char *message);
+void	parse_dollar(t_command *head);
+int		ft_strcmp(const char *s1, const char *s2);
+void	split_built_in(t_command *head);
+void	print_command_list(t_command *cmd);
+void	update_env(t_command *iter, char *key, char *new_value);
+char	*find_env_value(t_command *iter, char *key);
+char	*get_env_value(char *var_name, t_envp *env_list);
+void	echo_function(t_command *iter);
+
+
+
+
+
+
+
+
+// // Environment linked list functions
+// t_envp *create_env_list(char **envp);
+// char *find_env_value(t_envp *env_list, char *key);
+// void add_env_node(t_envp **head, char *key, char *value);
+// void update_env_value(t_envp *env_list, char *key, char *value);
+// void free_env_list(t_envp *env_list);
+
+#endif
